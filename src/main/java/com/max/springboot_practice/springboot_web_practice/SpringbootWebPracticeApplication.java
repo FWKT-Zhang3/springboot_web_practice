1af3482fd5d7eb90f42255e0549dc2067b5b7f71
FWKT-Zhang3 <max.zhang2020@outlook.com>
2020-09-25 12:56:31 +1000

    Initialize

---
+++ src/main/java/com/max/springboot_practice/springboot_web_practice/SpringbootWebPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.max.springboot_practice.springboot_web_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/max/springboot_practice/springboot_web_practice/SpringbootWebPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.springboot_practice.springboot_web_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
